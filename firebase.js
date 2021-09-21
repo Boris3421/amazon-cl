4f9f3bab1ea1c3be48e9688bf393da32508122a3
Boris3421 <boris.raischi@gmail.com>
2021-09-21 16:25:40 +0200

    app built

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD7UPiyME216j3dq5IZ8W5soUsDsNdQnNg",
+    authDomain: "due-636b1.firebaseapp.com",
+    projectId: "due-636b1",
+    storageBucket: "due-636b1.appspot.com",
+    messagingSenderId: "358013675732",
+    appId: "1:358013675732:web:6d3e49f3d93c6d9df6f341",
+    measurementId: "G-KGWWDBGGG2"
+  };
